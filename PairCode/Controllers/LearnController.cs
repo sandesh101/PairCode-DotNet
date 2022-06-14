fc1c2b42c94ef224041e1e582899fbd3bdf57d7a
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-06-14 21:12:14 +0545

    Made Learn With Other Page

---
+++ PairCode/Controllers/LearnController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PairCode.Controllers
+{
+    public class LearnController : Controller
+    {
+        public ViewResult Learnwithfriend()
+        {
+            return View();
+        }
+    }
+}
