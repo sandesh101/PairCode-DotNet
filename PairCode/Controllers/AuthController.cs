bc8e45abbcaef917a92499d12582721aacaa3daf
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-06-14 16:22:43 +0545

    Initial commit

---
+++ PairCode/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PairCode.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult Signup()
+        {
+            return View();
+        }
+    }
+}
