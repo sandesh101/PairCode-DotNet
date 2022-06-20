ca8473b61c8133736d7120b94081bc2b2fc577b0
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-06-20 23:34:22 +0545

    Made Course Page

---
+++ PairCode/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PairCode.Controllers
+{
+    public class CourseController : Controller
+    {
+       public ViewResult Course()
+        {
+            return View();
+        }
+    }
+}
